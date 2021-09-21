b7988fdf3490d7a9f2bc37e6482e7f1cbb6a16e3
Ilja Tollu <tollyu@lanit.ru>
2021-09-21 23:17:14 +0300

    add api/shorten endpoint

--- internal/app/server_test.go
+++ internal/app/server_test.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -44,6 +45,31 @@ func TestHandlePostLongURL(t *testing.T) {
 
 	require.Equal(t, http.StatusCreated, res.StatusCode, "status code")
 	require.Equal(t, string(body), "http://localhost:8080/123")
+}
+
+func TestHandlePostApiShorten(t *testing.T) {
+	rw := httptest.NewRecorder()
+	testRawLongURL := "http://test.com"
+	testLongURLJson := fmt.Sprintf(`{"url": "%s"}`, testRawLongURL)
+	req := httptest.NewRequest(
+		http.MethodPost,
+		"/api/shorten",
+		bytes.NewBufferString(testLongURLJson),
+	)
+	repo := new(RepositoryMock)
+	url, _ := url.Parse(testRawLongURL)
+	sh := NewURLShortener(repo)
+	repo.On("SaveURL", *url).Return(123)
+
+	sh.ServeHTTP(rw, req)
+
+	res := rw.Result()
+	defer res.Body.Close()
+	body, errBody := ioutil.ReadAll(res.Body)
+	require.NoError(t, errBody)
+
+	require.Equal(t, http.StatusCreated, res.StatusCode, "status code")
+	require.JSONEq(t, string(body), `{"result": "http://localhost:8080/123"}`)
 }
 
 func TestHandleGetShortUrl(t *testing.T) {
