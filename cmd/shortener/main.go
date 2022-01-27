b810cc743726e3f885016a21765115afa8f0188f
Emus88 <e.musrepov@gmail.com>
2022-01-27 23:12:16 +0700

    asfsdf

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
 
 	"github.com/EMus88/go-musthave-shortener-tpl/configs"
+	"github.com/EMus88/go-musthave-shortener-tpl/internal/app/service"
+	"github.com/EMus88/go-musthave-shortener-tpl/internal/handler"
+	"github.com/EMus88/go-musthave-shortener-tpl/internal/repository"
 	"github.com/gin-gonic/gin"
 
 	_ "github.com/lib/pq"
@@ -12,30 +17,30 @@ import (
 func main() {
 
 	config := configs.NewConfig()
-	//	ctx := context.Background()
-	//	db, err := repository.NewDBClient(ctx, config)
-	//	if err != nil {
-	//		log.Fatal(err)
-	//	}
 
-	//	repository.Migration(config)
-	//
-	//	r := repository.NewStorage(db)
-	//	s := service.NewService(r, config)
-	//	h := handler.NewHandler(s)
+	db, err := repository.NewDBClient(context.TODO(), config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repository.Migration(config)
+
+	r := repository.NewStorage(db)
+	s := service.NewService(r, config)
+	h := handler.NewHandler(s)
 
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
+
 	//router.Use(gin.Logger())
 	//router.Use(gin.Recovery())
 
 	//router.Use(handler.AuthMiddleware(h))
-
 	router.GET("/:id", func(c *gin.Context) { c.String(http.StatusBadRequest, "Not allowed requset") })
 	//router.GET("user/urls")
 	//router.GET("api/shorten/batch")
 	//router.GET("/ping", h.HandlerPingDB)
-	//router.POST("/", h.HandlerPost)
+	router.POST("/", h.HandlerPost)
 	//	router.POST("/api/shorten", h.HandlerPost)
 	//	router.NoRoute(func(c *gin.Context) { c.String(http.StatusBadRequest, "Not allowed requset") })
 
