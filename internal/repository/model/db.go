cde952950e58852cdccf8d916961b5c0918da1b1
Emus88 <e.musrepov@gmail.com>
2022-01-29 19:37:04 +0700

    dfgdfgdfg

--- internal/repository/model/db.go
+++ internal/repository/model/db.go
@@ -9,6 +9,6 @@ type Shorten struct {
 	ID        int    `gorm:"primary_key;auto_increment;unique" json:"-"`
 	URLID     string `gorm:"type:varchar(255);not null;unique" json:"-"`
 	ShortURL  string `gorm:"type:varchar(255);not null" json:"short_url"`
-	LongURL   string `gorm:"type:varchar(255);not null" json:"original_ url"`
+	LongURL   string `gorm:"type:varchar(255);not null" json:"original_url"`
 	SessionID int    `gorm:"type:int;not null" json:"-"`
 }
