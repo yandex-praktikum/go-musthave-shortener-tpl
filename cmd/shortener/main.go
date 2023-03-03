cb9797b95b885c8203de6f120c4dd16be970e80e
Fe-Nik-S <mikifeynman@gmail.com>
2023-03-03 23:21:09 +0300

    Initial commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,26 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"math/rand"
+	"time"
+
+	"github.com/mr-fard4y/go-musthave-shortener-tpl/internal/app/server"
+	"github.com/mr-fard4y/go-musthave-shortener-tpl/internal/app/server/config"
+	"github.com/mr-fard4y/go-musthave-shortener-tpl/internal/app/storage"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	err := config.LoadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := storage.New()
+
+	r := server.NewRouter(s)
+
+	log.Fatal(r.Run(config.Config.ServerAddress))
+}
